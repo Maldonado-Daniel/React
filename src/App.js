3ad134a027ceb7e6fc6f7ef4c6cdb7ef4e3b37e5
Maldonado-Daniel <107147023+Maldonado-Daniel@users.noreply.github.com>
2022-11-07 18:11:17 -0300

    añadi estilos al navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import NavBar from './components/NavBar/NavBar';
+import ItemsListContainer from './components/NavBar/CartWidget/ItemsListContainer/ItemsListContainer';
 import './App.css';
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <ItemsListContainer name="Bienvenidos a mi tienda online"/>
     </>
   );
 }
